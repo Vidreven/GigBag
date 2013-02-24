730e2f99e05dc7e83a670b22488845aa6c23be5a
Vladimir Adamić <vladimir.adamic@gmail.com>
2013-02-24 13:52:43 +0100

    Added event recommendations

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe HomeController do
 
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
+  describe "GET index" do
+    it "shows list of events with most popular_bands" do
+      bands = 50.times do FactoryGirl.create(:random_band) end
+      get :index
       response.should be_success
     end
   end
--- spec/factories/bands.rb
+++ spec/factories/bands.rb
@@ -1,5 +1,5 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
+require "faker"
 FactoryGirl.define do
   factory :band do
     name "Opeth"
@@ -8,6 +8,11 @@ FactoryGirl.define do
 
   factory :invalid_band, parent: :band do
     name nil
+    image "http://userserve-ak.last.fm/serve/500/58121627/Opeth.png"
+  end
+
+  factory :random_band, parent: :band do
+    name 
     image "http://userserve-ak.last.fm/serve/500/58121627/Opeth.png"
   end
 end
