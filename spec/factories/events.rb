8905f33ad8383da54a2c491d61e8d9a372e977f9
Vidreven <vidreven@yahoo.com>
2013-04-21 17:51:38 +0200

    Finished controller tests

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,6 +4,12 @@ FactoryGirl.define do
   factory :event do
     name "Testni event"
     event_date "2013-03-30"
-    event_time "2013-03-30 18:46:37"
+    event_time "18:46:37"
+  end
+
+  factory :invalid_event, parent: :event do
+  	name nil
+  	event_date "2013-03-30"
+    event_time "18:46:37"
   end
 end
